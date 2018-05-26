83e34625f21209b5afa6b25d31db7d8f23f6cb18
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2018-05-26 15:35:08 -0400

    offload qr code scan to web worker

--- client/utils/qrcode.js
+++ client/utils/qrcode.js
@@ -1,10 +1,8 @@
-import QrCode from 'qrcode-reader';
-
 /**
  * Convert QRcode data into a grocery item
- * 
+ *
  * @example
- * 
+ *
  *    let qrString = '1,Bananna,Fruit,/images/1.png,3.99,each';
  *    let groceryItem = qrCodeStringToObject(qrString)
  *    {
@@ -15,7 +13,7 @@ import QrCode from 'qrcode-reader';
  *      price: 3.99
  *      unit: 'each
  *    }
- * 
+ *
  * @public
  * @param {String} qrDataString QR code data
  * @return {Object} grocery item corresponding to QR code data
@@ -28,14 +26,14 @@ export function qrCodeStringToObject(qrDataString) {
     category,
     imageUrl,
     unit,
-    price: parseFloat(price)
-  }
+    price: parseFloat(price),
+  };
 }
 
 /**
  * Read an image file into an image buffer.
  * NOTE: This may be important for image processing inside a web worker
- * 
+ *
  * @public
  * @param {File} file file for QRcode image (i.e., from an <input type="file" /> )
  * @return {Promise<ImageData>} image buffer pre-loaded with QRcode image
@@ -48,20 +46,23 @@ export function fileToImageBuffer(file) {
     }
     // Read file
     var reader = new FileReader();
-    reader.addEventListener('load', () => {
-      let img = new Image();
-      let canvas = document.createElement('canvas');
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        let ctx = canvas.getContext('2d');
-        ctx.drawImage(img,0,0);
-        let data = ctx.getImageData(0, 0, img.width, img.height);
-        resolve(data);
-      }
-      img.src = event.target.result;
-    }, false);
-
+    reader.addEventListener(
+      'load',
+      () => {
+        let img = new Image();
+        let canvas = document.createElement('canvas');
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          let ctx = canvas.getContext('2d');
+          ctx.drawImage(img, 0, 0);
+          let data = ctx.getImageData(0, 0, img.width, img.height);
+          resolve(data);
+        };
+        img.src = event.target.result;
+      },
+      false
+    );
 
     reader.readAsDataURL(file);
   });
@@ -71,29 +72,40 @@ export function fileToImageBuffer(file) {
  * Take an image buffer that's pre-populated with a QR code image
  * and process it using the qrcode-reader library. At the end, if all looks
  * well, add the item represented by the QRcode to the cart.
- * 
+ *
  * @public
- * @param {ImageData} imageBuffer 
+ * @param {ImageData} imageBuffer
  * @param {CartStore} cartStore
  * @return {Promise}
  */
 export function onQrCodeScan(imageBuffer, cartStore) {
-  return new Promise((resolve/*, reject*/) => {
-
+  return new Promise((resolve /*, reject*/) => {
     // BEGIN MAIN THREAD SOLUTION
-    let qr = new QrCode();
-    qr.callback = function(error, rawResult) {
-      if(error) {
-        self.postMessage({ error });
-        return;
-      }
-      let result = qrCodeStringToObject(rawResult.result);
-      resolve(result);
-    }
-    qr.decode(imageBuffer);
+    // let qr = new QrCode();
+    // qr.callback = function(error, rawResult) {
+    //   if(error) {
+    //     self.postMessage({ error });
+    //     return;
+    //   }
+    //   let result = qrCodeStringToObject(rawResult.result);
+    //   resolve(result);
+    // }
+    // qr.decode(imageBuffer);
     // END MAIN THREAD SOLUTION
-    
-  }).then((qrData) => {
+    //create a worker to handle the big processing for us
+    let worker = new Worker('qr-worker.js');
+    //send a message to the worker script
+    worker.postMessage(imageBuffer);
+    worker.onmessage = event => {
+      //console.log('event', event);
+      //the event message comes back with
+      //a property called data that we need
+      //to then grab our data property
+      //to resolve the promise with
+      resolve(event.data.data);
+    };
+  }).then(qrData => {
+    //console.log(qrData);
     cartStore.addItemToCart(qrData);
   });
 }
