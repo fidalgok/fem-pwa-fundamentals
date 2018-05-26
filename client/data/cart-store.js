282603a2e176f557602b81b71ad3ad06bd9d447a
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2018-05-26 18:51:16 -0400

    feature: add fetch post call to server to save cart and update on order submission

--- client/data/cart-store.js
+++ client/data/cart-store.js
@@ -11,7 +11,7 @@ export default class CartStore {
   /**
    * Instantiate a new CartStore
    * There's usually only one of these per app
-   * 
+   *
    * @public
    * @return {CartStore}
    */
@@ -26,19 +26,19 @@ export default class CartStore {
     /**
      * The _restoreCart() function is responsible for getting the "initial"
      * cart state. This may seem a little strange for now, but it's a "blank"
-     * we'll fill in later as we add in offline support 
+     * we'll fill in later as we add in offline support
      */
-    this._restoreCart().then((newItems) => {
+    this._restoreCart().then(newItems => {
       // once the promise returned by _restoreCart resolves
       this._items = newItems; // use the value as the contents of the cart
-      this._onItemsUpdated();  // notify anyone who may care about cart contents changing
+      this._onItemsUpdated(); // notify anyone who may care about cart contents changing
     });
   }
 
   /**
    * Get the array of items currently in the shopping cart.
    * This is a read-only array
-   * 
+   *
    * @public
    * @return {ReadonlyArray<Object>}
    */
@@ -49,51 +49,80 @@ export default class CartStore {
   /**
    * Get the "initial" contents of the cart.
    * For now the cart will start as an empty array, but we'll enhance this later!
-   * 
+   *
    * @private
    * @return {Promise<Array<Object>>}
    */
   _restoreCart() {
     return fetch(`${API_ENDPOINT}api/cart/items`)
-      .then((response) => {
+      .then(response => {
         if (response.ok) return response.json();
         else throw new Error('Problem fetching cart data');
       })
-      .then((jsonData) => jsonData.data);
+      .then(jsonData => jsonData.data);
   }
 
   /**
    * Persist the contents of the cart
    * For now this will only cause the UI to update, but we'll enhance it later
-   * 
+   *
    * @private
    * @return {Promise} the new cart
    */
   _saveCart() {
     this._onItemsUpdated();
-    return Promise.resolve(this.items);
+    return fetch(`${API_ENDPOINT}api/cart/items`, {
+      //need to pass options to our fetch to put the
+      //items in the cart on the server
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ data: this.items }),
+    })
+      .then(response => {
+        if (response.ok) return response.json();
+        else throw new Error('Problem fetching cart data');
+      })
+      .then(jsonData => jsonData.data);
+    // return Promise.resolve(this.items);
   }
 
   /**
    * "Check out" of the grocery store, turning the contents of the shopping cart
    * into an "order"
-   * 
+   *
    * @public
-   * @return {Promise} 
+   * @return {Promise}
    */
   doCheckout() {
-    throw 'CartStore#doCheckout Not yet implemented'
+    //make post to our checkout endpoint to submit
+    //an order
+    return fetch(`${API_ENDPOINT}api/order`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ data: this.items }),
+    })
+      .then(this._restoreCart)
+      .then(newItems => {
+        this._items = newItems;
+        this._onItemsUpdated();
+      });
+    // throw 'CartStore#doCheckout Not yet implemented';
   }
 
   /**
-   * 
-   * @param {Object} groceryItem 
+   *
+   * @param {Object} groceryItem
    */
   addItemToCart(groceryItem) {
     // check to see if this grocery item is already in the cart
-    let existingCartItem = this._items
-      .filter((ci) => `${ci.groceryItem.id}` === `${groceryItem.id}`)[0];
-    
+    let existingCartItem = this._items.filter(
+      ci => `${ci.groceryItem.id}` === `${groceryItem.id}`
+    )[0];
+
     if (existingCartItem) {
       // if it's already in the cart, increment its quantity
       existingCartItem.qty++;
@@ -101,7 +130,7 @@ export default class CartStore {
       // if it's not yet in the cart, add it to the cart
       let newItem = {
         groceryItem,
-        qty: 1
+        qty: 1,
       };
       this._items = this._items.concat(newItem);
     }
@@ -113,15 +142,16 @@ export default class CartStore {
    * Remove a grocery item from the cart.
    * Depending on the quantity of this item in the cart, the quantity may be decremented
    * or the item may be removed entirely.
-   * @param {Object} groceryItem 
+   * @param {Object} groceryItem
    */
   removeItemFromCart(groceryItem) {
     // find an existing object in the cart corresponding to this grocery item
-    let existingCartItem = this._items
-      .filter((ci) => ci.groceryItem.id === groceryItem.id)[0];
-    
+    let existingCartItem = this._items.filter(
+      ci => ci.groceryItem.id === groceryItem.id
+    )[0];
+
     if (!existingCartItem) return; // nothing was in the cart to begin with
-    
+
     // if the existing item found has a quantity > 1
     if (existingCartItem.qty > 1) {
       // decrement the quantity
@@ -129,7 +159,7 @@ export default class CartStore {
     } else {
       // otherwise (i.e., quantity is 1) remove the object from the cart entirely
       // find its index
-      let idx = this._items.findIndex((i) => i.groceryItem.id === groceryItem.id);
+      let idx = this._items.findIndex(i => i.groceryItem.id === groceryItem.id);
       // remove the object in the cart at that index
       this._items.splice(idx, 1);
     }
@@ -139,7 +169,7 @@ export default class CartStore {
 
   /**
    * Notify any registered listeners that the cart items have changed
-   * 
+   *
    * @private
    * @return {void}
    */
